751d994a56c3fc41ef91e0866cfd2c689b7d7f9f
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2022-03-21 23:04:31 +0200

    Add AutoMapper

---
+++ Src/GoSport.Core/AutoMapper/AutoMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using GoSport.Core.ViewModel.User;
+using GoSport.Infrastructure.Data.DateModels;
+
+namespace GoSport.Core.AutoMapper
+{
+    public class AutoMapper : Profile
+    {
+        public AutoMapper()
+        {
+            this.CreateMap<User, RegisterViewModel>().ReverseMap();
+        }
+    }
+}
