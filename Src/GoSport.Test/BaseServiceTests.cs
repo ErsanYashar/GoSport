0bef936cb364da223bb644c1c4e049abb491539e
ErsanYashar <76754048+ErsanYashar@users.noreply.github.com>
2022-04-09 13:28:38 +0300

    Add GoSport test project and add class BaseServiceTests

---
+++ Src/GoSport.Test/BaseServiceTests.cs
@@ -0,0 +1,58 @@
+﻿using GoSport.Core.Services;
+using GoSport.Core.Services.Interfaces;
+using GoSport.Infrastructure.Data;
+using GoSport.Infrastructure.Data.DateModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace GoSport.Test
+{
+    public class BaseServiceTests
+    {
+        protected BaseServiceTests()
+        {
+            var serviceProvider = new ServiceCollection().AddEntityFrameworkInMemoryDatabase().BuildServiceProvider();
+
+            var services = new ServiceCollection();
+            services.AddSingleton<IConfiguration>(new ConfigurationBuilder().Build());
+            services.AddOptions();
+            services.AddDbContext<ApplicationDbContext>(b => b.UseInMemoryDatabase("ApplicationDbContext").UseInternalServiceProvider(serviceProvider));
+
+
+            services.AddDefaultIdentity<User>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 5;
+                options.Password.RequiredUniqueChars = 0;
+                options.User.RequireUniqueEmail = false;
+            });
+
+            services.AddTransient<ITown, TownService>();
+            services.AddTransient<IUsersService, UsersService>();
+            services.AddTransient<ISportsService, SportsService>();
+            services.AddTransient<IDisciplinesService, DisciplinesService>();
+            services.AddTransient<IMessagesService, MessagesService>();
+            services.AddTransient<IOrganizersService, OrganizersService>();
+            services.AddTransient<IVenuesService, VenuesService>();
+            services.AddTransient<IEventsService, EventsService>();
+
+            var context = new DefaultHttpContext();
+
+            services.AddSingleton<IHttpContextAccessor>(
+               new HttpContextAccessor()
+               {
+                   HttpContext = context,
+               });
+
+            this.ServiceProvider = services.BuildServiceProvider();
+        }
+
+        public IServiceProvider ServiceProvider { get; set; }
+    }
+}
---
+++ Src/GoSport.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace GoSport.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
